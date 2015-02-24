31780ba18cbf950f72691725297868d11013883f
Wei Chen <wei.chen@spsmail.cuny.edu>
2015-02-24 22:32:10 +0000

    created task 3

---
+++ task_03.py
@@ -0,0 +1,13 @@
+#!usr/bin/env python
+# _*_ coding: utf-8 _*_
+"""Task 3"""
+
+
+Name = raw_input('What is your name? ')
+Loan = raw_input('What is the amount of your principle? ')
+Loan = int(Loan)
+Years = raw_input('For how many years is this loan being borrowed? ')
+Years = int(Years)
+Qulification = raw_input('Are you pre-qualified for this loan? ')
+
+
